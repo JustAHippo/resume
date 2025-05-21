fdd2a372c4d20f7ee1dd44c95398629e0bce2fbd
JustAHippo <82006314+JustAHippo@users.noreply.github.com>
2025-05-21 17:24:52 -0400

    Add frontend procedures for sending mail

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import {
   Form,
   Button,
   Card,
+  CardHeader,
   Input,
   CardBody,
   Select,
   SelectItem,
+  Alert,
   Textarea,
 } from "@heroui/react";
 
@@ -24,13 +27,41 @@ const reasons = [
 
 export default function ContactPage() {
   const [submitted, setSubmitted] = React.useState(null);
-
-  const onSubmit = (e: any) => {
+  const [captchaValue, setCaptchaValue] = React.useState(null);
+   const [topAlert, setTopAlert] = React.useState(<div></div>);
+  const handleCaptchaChange = (value : any) => {
+        setCaptchaValue(value);
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const data: any = Object.fromEntries(new FormData(e.currentTarget));
+    const formData: any = Object.fromEntries(new FormData(e.currentTarget));
+    console.log(formData)
+    if (captchaValue == "" || captchaValue == null) {
+      setTopAlert(<div><Alert className="text-wrap" color="danger">You must complete the captcha first!</Alert></div>)
+      return
+    }
+    try {
+      const response = await fetch('/api/v1/contact/send', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
 
-    setSubmitted(data);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Success:', data);
+      setTopAlert(<div><Alert className="text-wrap" color="success">Received! Check your email to verify. Your ticket will expire in 1 hour.</Alert></div>);
+    } catch (error : any) {
+      setTopAlert(<div><Alert className="text-wrap" color="danger">{error.toString()}</Alert></div>);
+      console.error('Error:', error);
+    }
+    setSubmitted(formData);
   };
 
   return (
@@ -39,7 +70,11 @@ export default function ContactPage() {
       <h2 className="text-default-400 m-4">
         Here's where you can get in contact!
       </h2>
+      
       <Card className="">
+        <CardHeader className="w-full max-w-xs">
+          {topAlert}
+        </CardHeader>
         <CardBody>
           <Form className="w-full max-w-xs" onSubmit={onSubmit}>
             <Input
@@ -53,6 +88,7 @@ export default function ContactPage() {
             <Select
               isRequired
               className="max-w-xs"
+              name="subject"
               items={reasons}
               label="Reason for Communicating"
               placeholder="Include Subject"
@@ -62,8 +98,14 @@ export default function ContactPage() {
             <Textarea
               isRequired
               className="max-w-xs"
+              name="message"
               label="Message"
               placeholder="What would you like to say?"
+            />
+            <ReCAPTCHA
+            
+            sitekey="6Le0rUMrAAAAAH9T_ObN-V-8w24cBhaKrfhq-Yfk"
+            onChange={handleCaptchaChange}
             />
             <Button type="submit">Send</Button>
           </Form>
